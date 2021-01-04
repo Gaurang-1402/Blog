108c72b28c44c1a8f1f5ddd8a13d353f6ed793e0
Gaurang Ruparelia <gr2159@nyu.edu>
2021-01-04 09:55:59 -0500

    Added gitignore, procfile and update parts

--- app.js
+++ app.js
@@ -116,9 +116,13 @@ app.get("/posts/:day", function(req, res) {
 
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
